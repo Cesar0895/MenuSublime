961b5e6680d8daeaeb2d277aa8c5379a46a3666e
Cesar Espino <danger_95@hotmail.es>
2018-04-14 19:04:10 -0600

    Lista de artistas

--- app/src/main/java/com/example/cesarramses/menusublime/MainActivity.java
+++ app/src/main/java/com/example/cesarramses/menusublime/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.cesarramses.menusublime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lsvMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lsvMenu=findViewById(R.id.lsvMenu);
+        ArrayList<String> artistas=new ArrayList<String>();
+        artistas.add("Carvajal en concierto");
+        artistas.add("Metallica en concierto");
+        artistas.add("Ice Cube en concierto");
+        artistas.add("Angus Young en concierto");
+        artistas.add("Iron Maiden en concierto");
+
+        //se establece un puente entre el ArrayList y el Listview
+        ArrayAdapter<String> adaptador=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,artistas);
+        lsvMenu.setAdapter(adaptador);
     }
 }
